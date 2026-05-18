3cfa90eb6758cf86c6d4430e67c001fb1528826c
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-05-18 23:02:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,45 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+// ======================================================
+// MIDDLEWARES
+// ======================================================
+
+app.use(express.json());
+
+app.use(cors({
+  origin: "https://yamaha-sty-webs.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+// ======================================================
+// TEST ROUTE
+// ======================================================
 
 app.get("/", (req, res) => {
+
   res.json({
     success: true,
     message: "Server running"
   });
+
 });
+
+// ======================================================
+// ROUTES
+// ======================================================
+
+// app.use("/auth", require("./routes/Auth"));
+
+// ======================================================
+// PORT
+// ======================================================
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
+
   console.log("SERVER STARTED ON", PORT);
+
 });
